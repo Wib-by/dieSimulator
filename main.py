c17164b50eb020a31e1847467e266dcfad94dfd4
Sean <27dzk8dqh6@privaterelay.appleid.com>
2026-03-10 21:07:00 +0800

    update

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ Enter a limit (Default - 6): """)
 try:
     limit = int(prim)
     result = randint(1, limit)
-    print(result)
+    print(f"\n{result}")
 except ValueError:
-    if limit == "":
+    if prim == "":
         result = randint(1, 6)
-        print(result)
+        print(f"\n{result}")
     else:
         print("User error - invalid input")
